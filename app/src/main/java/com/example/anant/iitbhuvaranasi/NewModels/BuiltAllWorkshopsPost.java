7ca27dff85b787adcc5ee92cf49d6d12bfe63f3e
mohit-mangal <mohitmangal.ece18@itbhu.ac.in>
2020-05-17 01:29:02 +0530

    all workshops model

---
+++ app/src/main/java/com/example/anant/iitbhuvaranasi/NewModels/BuiltAllWorkshopsPost.java
@@ -0,0 +1,15 @@
+package com.example.anant.iitbhuvaranasi.NewModels;
+
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class BuiltAllWorkshopsPost {
+
+    @Nullable
+    List<BuiltWorkshopSummaryPost>  active_workshops;
+
+    @Nullable
+    List<BuiltWorkshopSummaryPost>  past_workshops;
+
+}
