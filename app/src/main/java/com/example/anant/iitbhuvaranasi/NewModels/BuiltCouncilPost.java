d37cc6edefc165d9db7b8dfe1acf42ea024413db
mohit-mangal <mohitmangal.ece18@itbhu.ac.in>
2020-05-17 01:02:10 +0530

    council detail model

---
+++ app/src/main/java/com/example/anant/iitbhuvaranasi/NewModels/BuiltCouncilPost.java
@@ -0,0 +1,53 @@
+package com.example.anant.iitbhuvaranasi.NewModels;
+
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class BuiltCouncilPost {
+
+    @Nullable
+    private int id;
+
+    @Nullable
+    private String name;
+
+    @Nullable
+    private String description;
+
+    @Nullable
+    private SecyPost gensec;
+
+    @Nullable
+    private List<SecyPost> joint_gensec;
+
+    @Nullable
+    private List<ClubListPost> clubs;
+
+    @Nullable
+    private String small_image_url;
+
+    @Nullable
+    private String large_image_url;
+
+    @Nullable
+    private boolean is_por_holder;
+
+    @Nullable
+    private String website_url;
+
+    @Nullable
+    private String facebook_url;
+
+    @Nullable
+    private String twitter_url;
+
+    @Nullable
+    private String instagram_url;
+
+    @Nullable
+    private String linkedin_url;
+
+    @Nullable
+    private String youtube_url;
+}
