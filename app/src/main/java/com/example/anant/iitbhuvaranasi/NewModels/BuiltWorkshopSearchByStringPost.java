6dfea93a95c0d016034ce6f6160413507d6dc3ef
mohit-mangal <mohitmangal.ece18@itbhu.ac.in>
2020-05-17 01:35:24 +0530

    workshop search model

---
+++ app/src/main/java/com/example/anant/iitbhuvaranasi/NewModels/BuiltWorkshopSearchByStringPost.java
@@ -0,0 +1,13 @@
+package com.example.anant.iitbhuvaranasi.NewModels;
+
+import androidx.annotation.Nullable;
+
+public class BuiltWorkshopSearchByStringPost {
+
+    @Nullable
+    private String search_by;
+
+    @Nullable
+    private String search_string;
+
+}
