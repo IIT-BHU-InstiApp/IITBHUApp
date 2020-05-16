05374cf1b69aa163acbbf816037d68da559b0d55
mohit-mangal <mohitmangal.ece18@itbhu.ac.in>
2020-05-17 01:23:15 +0530

    search tag model

---
+++ app/src/main/java/com/example/anant/iitbhuvaranasi/NewModels/TagSearch.java
@@ -0,0 +1,16 @@
+package com.example.anant.iitbhuvaranasi.NewModels;
+
+import androidx.annotation.Nullable;
+
+public class TagSearch {
+
+    @Nullable
+    private int id;
+
+    @Nullable
+    private String tag_name;
+
+    @Nullable
+    private int club;
+
+}
