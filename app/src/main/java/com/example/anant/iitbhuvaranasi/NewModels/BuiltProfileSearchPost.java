7cf54e0a805e429aa923a2a5c49b60f444566ae1
mohit-mangal <mohitmangal.ece18@itbhu.ac.in>
2020-05-17 01:48:30 +0530

    search profile model

---
+++ app/src/main/java/com/example/anant/iitbhuvaranasi/NewModels/BuiltProfileSearchPost.java
@@ -0,0 +1,14 @@
+package com.example.anant.iitbhuvaranasi.NewModels;
+
+import androidx.annotation.Nullable;
+
+public class BuiltProfileSearchPost {
+
+    @Nullable
+    private String search_by;
+
+    @Nullable
+    private String search_string;
+
+
+}
