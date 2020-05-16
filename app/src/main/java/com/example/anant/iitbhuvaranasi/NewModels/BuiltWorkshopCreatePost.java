6ffb1fd409f40c0a59bb9388d95ef3e8887dd677
mohit-mangal <mohitmangal.ece18@itbhu.ac.in>
2020-05-17 01:39:53 +0530

    create workshop model

---
+++ app/src/main/java/com/example/anant/iitbhuvaranasi/NewModels/BuiltWorkshopCreatePost.java
@@ -0,0 +1,51 @@
+package com.example.anant.iitbhuvaranasi.NewModels;
+
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class BuiltWorkshopCreatePost {
+
+    private String title;
+
+    private int club;
+
+    private String date;
+
+    @Nullable
+    private int id;
+
+    @Nullable
+    private String description;
+
+    @Nullable
+    private String time;
+
+    @Nullable
+    private String location;
+
+    @Nullable
+    private String latitude;
+
+    @Nullable
+    private String longitude;
+
+    @Nullable
+    private String audience;
+
+    @Nullable
+    private List<Integer> resources;
+
+    @Nullable
+    private List<Integer> contacts;
+
+    @Nullable
+    private String image_url;
+
+    @Nullable
+    private List<Integer> tags;
+
+    @Nullable
+    private String link;
+
+}
