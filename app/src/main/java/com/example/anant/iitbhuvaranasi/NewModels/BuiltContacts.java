ea3cedc2d3b7cc23ac5137827a77d625e81e3472
mohit-mangal <mohitmangal.ece18@itbhu.ac.in>
2020-05-17 01:54:01 +0530

    post contact and tag models

---
+++ app/src/main/java/com/example/anant/iitbhuvaranasi/NewModels/BuiltContacts.java
@@ -0,0 +1,11 @@
+package com.example.anant.iitbhuvaranasi.NewModels;
+
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class BuiltContacts {
+
+    @Nullable
+    private List<Integer> contacts;
+}
