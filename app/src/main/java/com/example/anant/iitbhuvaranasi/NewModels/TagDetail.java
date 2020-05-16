ed8a85b27985c250f101e7f824a8b9ef604bd2b7
mohit-mangal <mohitmangal.ece18@itbhu.ac.in>
2020-05-17 01:24:28 +0530

    tag detail  model

---
+++ app/src/main/java/com/example/anant/iitbhuvaranasi/NewModels/TagDetail.java
@@ -0,0 +1,16 @@
+package com.example.anant.iitbhuvaranasi.NewModels;
+
+import androidx.annotation.Nullable;
+
+public class TagDetail {
+
+    @Nullable
+    private int id;
+
+    @Nullable
+    private String tag_name;
+
+    @Nullable
+    private ClubListPost club;
+
+}
