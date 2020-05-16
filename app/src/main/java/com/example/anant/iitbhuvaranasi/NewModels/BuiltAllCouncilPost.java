5e9c0f8d06eaddfff7c4e24cd0c96e16370216ca
mohit-mangal <mohitmangal.ece18@itbhu.ac.in>
2020-05-17 00:41:32 +0530

    council Summary model

---
+++ app/src/main/java/com/example/anant/iitbhuvaranasi/NewModels/BuiltAllCouncilPost.java
@@ -0,0 +1,18 @@
+package com.example.anant.iitbhuvaranasi.NewModels;
+
+import androidx.annotation.Nullable;
+
+public class BuiltAllCouncilPost {
+
+    @Nullable
+    private int id;
+
+    @Nullable
+    private String name;
+
+    @Nullable
+    private String small_image_url;
+
+    @Nullable
+    private String large_image_url;
+}
