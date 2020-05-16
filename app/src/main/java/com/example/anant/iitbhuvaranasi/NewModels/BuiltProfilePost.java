b2c462496fb6f5eb7f153ac00cbbb7ddf494a375
mohit-mangal <mohitmangal.ece18@itbhu.ac.in>
2020-05-17 01:20:20 +0530

    profile model

---
+++ app/src/main/java/com/example/anant/iitbhuvaranasi/NewModels/BuiltProfilePost.java
@@ -0,0 +1,36 @@
+package com.example.anant.iitbhuvaranasi.NewModels;
+
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class BuiltProfilePost {
+
+    @Nullable
+    private int id;
+
+    @Nullable
+    private String name;
+
+    @Nullable
+    private String email;
+
+    @Nullable
+    private String phone_number;
+
+    @Nullable
+    private String department;
+
+    @Nullable
+    private String year_of_joining;
+
+    @Nullable
+    private List<ClubListPost> subscriptions;
+
+    @Nullable
+    private List<ClubListPost> club_privileges;
+
+    @Nullable
+    private String photo_url;
+
+}
