13ee4f80235a11b0245682605e7fa40049257e91
mohit-mangal <mohitmangal.ece18@itbhu.ac.in>
2020-05-17 01:51:37 +0530

    team model

---
+++ app/src/main/java/com/example/anant/iitbhuvaranasi/NewModels/BuiltTeamMemberPost.java
@@ -0,0 +1,14 @@
+package com.example.anant.iitbhuvaranasi.NewModels;
+
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class BuiltTeamMemberPost {
+    @Nullable
+    private String  role;
+
+    @Nullable
+    private List<TeamMember> team_members;
+
+}
---
+++ app/src/main/java/com/example/anant/iitbhuvaranasi/NewModels/TeamMember.java
@@ -0,0 +1,16 @@
+package com.example.anant.iitbhuvaranasi.NewModels;
+
+import androidx.annotation.Nullable;
+
+public class TeamMember {
+
+    @Nullable
+    private String name;
+
+    @Nullable
+    private String github_username;
+
+    @Nullable
+    private String github_image_url;
+
+}
