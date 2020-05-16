4f025f13864c321e01fc289707f2fe13d1b161c6
mohit-mangal <mohitmangal.ece18@itbhu.ac.in>
2020-05-17 01:22:19 +0530

    create tag model

---
+++ app/src/main/java/com/example/anant/iitbhuvaranasi/NewModels/TagCreate.java
@@ -0,0 +1,16 @@
+package com.example.anant.iitbhuvaranasi.NewModels;
+
+import androidx.annotation.Nullable;
+
+public class TagCreate {
+
+    @Nullable
+    private int id;
+
+    @Nullable
+    private String tag_name;
+
+    @Nullable
+    private int club;
+
+}
