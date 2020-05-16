331f532b035156fec1263cffd48b0bc3c5e4f57f
mohit-mangal <mohitmangal.ece18@itbhu.ac.in>
2020-05-17 01:32:29 +0530

    workshop detail model

---
+++ app/src/main/java/com/example/anant/iitbhuvaranasi/NewModels/BuiltWorkshopDetailPost.java
@@ -0,0 +1,66 @@
+package com.example.anant.iitbhuvaranasi.NewModels;
+
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class BuiltWorkshopDetailPost {
+
+    @Nullable
+   private int  id;
+
+    @Nullable
+    private    String  title;
+
+    @Nullable
+    private   String  description;
+
+    @Nullable
+    private   ClubListPost  club;
+
+    @Nullable
+    private   String  date;
+
+    @Nullable
+    private   String  time;
+
+    @Nullable
+    private   String  location;
+
+    @Nullable
+    private   String  latitude;
+
+    @Nullable
+    private   String  longitude;
+
+    @Nullable
+    private   String  audience;
+
+    @Nullable
+    private   List<WorkshopResources> resources;
+
+    @Nullable
+    private   List<ContactPost>  contacts;
+
+    @Nullable
+    private   String  image_url;
+
+    @Nullable
+    private   boolean  is_interested;
+
+    @Nullable
+    private   int  interested_users;
+
+    @Nullable
+    private   boolean  is_workshop_contact;
+
+    @Nullable
+    private   boolean  is_por_holder;
+
+    @Nullable
+    private   List<TagDetail>  tags;
+
+    @Nullable
+    private   String  link;
+
+}
