4ee84a696ed61a596c203b24ebceb0410484431e
mohit-mangal <mohitmangal.ece18@itbhu.ac.in>
2020-05-17 00:40:05 +0530

    Club summary model

---
+++ app/src/main/java/com/example/anant/iitbhuvaranasi/NewModels/ClubListPost.java
@@ -0,0 +1,22 @@
+package com.example.anant.iitbhuvaranasi.NewModels;
+
+import androidx.annotation.Nullable;
+
+public class ClubListPost {
+
+    @Nullable
+    private int id;
+
+    @Nullable
+    private String name;
+
+    @Nullable
+    private BuiltAllCouncilPost council;
+
+    @Nullable
+    private String small_image_url;
+
+    @Nullable
+    private String large_image_url;
+
+}
