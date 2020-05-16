8a044440d54f25773c681c21b7edfa745cffe853
mohit-mangal <mohitmangal.ece18@itbhu.ac.in>
2020-05-17 00:39:16 +0530

    Workshop summary model

---
+++ app/src/main/java/com/example/anant/iitbhuvaranasi/NewModels/BuiltWorkshopSummaryPost.java
@@ -0,0 +1,28 @@
+package com.example.anant.iitbhuvaranasi.NewModels;
+
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class BuiltWorkshopSummaryPost {
+
+    @Nullable
+    private int id;
+
+    @Nullable
+    private ClubListPost club;
+
+    @Nullable
+    private String title;
+
+    @Nullable
+    private String date;
+
+    @Nullable
+    private String time;
+
+    @Nullable
+    private List<TagDetail> tags;
+
+
+}
