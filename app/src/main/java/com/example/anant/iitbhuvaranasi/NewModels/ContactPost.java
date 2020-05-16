b922a891c00cd914c96a8fb738c069f89cc30275
mohit-mangal <mohitmangal.ece18@itbhu.ac.in>
2020-05-17 01:33:45 +0530

    contact model

---
+++ app/src/main/java/com/example/anant/iitbhuvaranasi/NewModels/ContactPost.java
@@ -0,0 +1,22 @@
+package com.example.anant.iitbhuvaranasi.NewModels;
+
+import androidx.annotation.Nullable;
+
+public class ContactPost {
+
+    @Nullable
+    private int id;
+
+    @Nullable
+    private String name;
+
+    @Nullable
+    private String email;
+
+    @Nullable
+    private String phone_number;
+
+    @Nullable
+    private String photo_url;
+
+}
