206a97608226b2746abaf6d0c85060b4c69cf30b
mohit-mangal <mohitmangal.ece18@itbhu.ac.in>
2020-05-17 01:25:57 +0530

    Workshop resources model

---
+++ app/src/main/java/com/example/anant/iitbhuvaranasi/NewModels/WorkshopResources.java
@@ -0,0 +1,19 @@
+package com.example.anant.iitbhuvaranasi.NewModels;
+
+import androidx.annotation.Nullable;
+
+public class WorkshopResources {
+
+    @Nullable
+    private int id;
+
+    @Nullable
+    private String name;
+
+    @Nullable
+    private String link;
+
+    @Nullable
+    private String resource_type;
+
+}
